4b2a22cafe0a2eedf56cdb98248e0422b1062b9e
s3codecl <gandradev@gmail.com>
2024-01-23 01:47:56 -0300

    variables - while - condición - contador - cantidad de intentos

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número por favor:");
+//Variables
+let numeroSecreto = 5;
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
 
-console.log(numeroUsuario);
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el número');
+    console.log(numeroUsuario);    
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierta
+        intentos = intentos + 1;
+        palabraVeces = 'veces';
+        if (intentos > 3) {
+            alert('Llegaste al número máximo de intentos');
+            break;
+        }
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
+    }
 }
